5d80f29261f96f8ad2ab38451939ddb05d080669
Jendoliver <alvarezmonllor@gmail.com>
2017-10-07 16:51:58 +0200

    Practica1-RandomNums finishedgit add .

--- Practica1-RandomNums/Practica1-RandomNums/Vista.swift
+++ Practica1-RandomNums/Practica1-RandomNums/Vista.swift
@@ -16,55 +16,96 @@ class ViewController: UIViewController
     @IBOutlet weak var btn4: UIButton!
     @IBOutlet weak var btn5: UIButton!
     @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btnPlayAgain: UIButton!
     
     @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var labelLevel: UILabel!
+    @IBOutlet weak var labelPoints: UILabel!
+    @IBOutlet weak var labelMain: UILabel!
     
     @IBAction func touchBtn(_ sender: UIButton!)
     {
         if(Int(sender.currentTitle!) == numbers[correctButtonIndex]) {
             sender.isHidden = true;
+            sender.isEnabled = false;
             correctButtonIndex += 1;
+            points += 1;
+            labelPoints.text = String(points)
         }
-        if(correctButtonIndex == 5) {
-            level += 1;
-            time = 30 - 3 * level;
+        if(correctButtonIndex == 6) {
+            timer.invalidate();
+            if(level < 9) {
+                level += 1;
+            }
+            generateLevel();
         }
     }
+    @IBAction func playAgainBtn(_ sender: UIButton) {
+        level = 0;
+        points = 0;
+        labelPoints.text = String(points);
+        btnPlayAgain.isHidden = true;
+        btnPlayAgain.isEnabled = false;
+        labelMain.text = "Sort the numbers fam!";
+        generateLevel();
+    }
     
-    var buttons : [UIButton] = [];
-    var numbers : [Int] = [];
-    var correctButtonIndex : Int = 0;
-    var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
-    var time : Int = 30;
-    var level : Int = 0;
+    var buttons : [UIButton] = []; // Buttons array
+    var numbers : [Int] = []; // Numbers array
+    var correctButtonIndex : Int = 0; // The array index of the next correct button
+    var timer = Timer();
+    var time : Int = 30; // time display value
+    var level : Int = 0; // current level
+    var points : Int = 0; // player points
     
     override func viewDidLoad() // Init
     {
         buttons = [btn1, btn2, btn3, btn4, btn5, btn6];
-        super.viewDidLoad()
-        generateNumbers();
-        numbers.sort();
-        labelTime.text = String(time);
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.viewDidLoad();
+        generateLevel();
     }
     
-    func generateNumbers() {
+    func generateLevel()
+    {
         if( !numbers.isEmpty) {
             numbers.removeAll();
         }
         for i in 0...5 {
             numbers.append(Int(arc4random_uniform(201)) - 100);
             buttons[i].setTitle(String(numbers[i]), for: .normal);
+            buttons[i].isEnabled = true;
+            buttons[i].isHidden = false;
         }
+        numbers.sort();
+        time = 30 - 3 * level;
+        labelTime.text = String(time);
+        labelLevel.text = String(level+1);
+        correctButtonIndex = 0;
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(tick), userInfo: nil, repeats: true);
+    }
+    
+    func loseCondition() {
+        timer.invalidate();
+        for i in 0...5 {
+            buttons[i].isEnabled = false;
+            buttons[i].isHidden = true;
+        }
+        labelMain.text = "You lose!!!";
+        btnPlayAgain.isHidden = false;
+        btnPlayAgain.isEnabled = true;
     }
     
     func tick() {
         time -= 1;
+        if(time == 0) {
+            loseCondition();
+        }
         labelTime.text = String(time);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
 
